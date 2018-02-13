95ac28d77234160feead6480429726eee43b74d2
abdulazizsaleh <32594301+abdulazizsaleh@users.noreply.github.com>
2018-02-13 08:20:29 +0300

    added php files

---
+++ imports/navigation2.php
@@ -0,0 +1,29 @@
+<nav class="navbar navbar-inverse">
+  <div class="container-fluid">
+    <div class="navbar-header">
+      <a class="navbar-brand" href="#">ProjectSpot</a>
+    </div>
+    <ul class="nav navbar-nav">
+      <li class="active"><a href="#">Account</a></li>
+      <li><a href="#">MyProject</a></li>
+      <li><a href="#">MyTeam</a></li>
+      <li><a href="#">Mail Box</a></li>
+    </ul>
+
+
+    <ul class="nav navbar-nav navbar-right">
+      <form class="navbar-form navbar-left" action="/action_page.php">
+        <div class="input-group">
+          <input type="text" class="form-control" placeholder="Search" name="search">
+          <div class="input-group-btn">
+            <button class="btn btn-default" type="submit">
+              <i class="glyphicon glyphicon-search"></i>
+            </button>
+          </div>
+        </div>
+      </form>
+      <!-- <li><a href="#"><span class="glyphicon glyphicon-user"></span> Sign Up</a></li> -->
+      <!-- <li><a href="#"><span class="glyphicon glyphicon-log-in"></span> Login</a></li> -->
+      </ul>
+    </div>
+</nav>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+  include 'imports/head.php';
+  include 'imports/navigation1.php';
+  /*include 'imports/navigation2.php';*/
+?>
+  </body>
+</html>
