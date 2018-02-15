b38087d28bc1e8007a15b63535098b7501130233
abdulazizsaleh <32594301+abdulazizsaleh@users.noreply.github.com>
2018-02-15 07:00:51 +0300

    separating news into a new file

---
+++ imports/news.php
@@ -0,0 +1,5 @@
+<div class="col-sm-4">
+  <img src="https://placehold.it/150x80?text=IMAGE" class="img-responsive" style="width:100%" alt="Image">
+  <h4 class="text-center">news topic</h3>
+  <p class="text-left text-justify">Imperdiet dui accumsan sit amet nulla facilisi morbi tempus iaculis. Vel facilisis volutpat est velit egestas dui. Lorem sed risus ultricies tristique nulla aliquet enim. Massa tempor nec feugiat nisl. <a href="#">click here</a></p>
+</div>
