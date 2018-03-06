9e3acbba653d0654b836ddeff4f1143b535d2ff7
abdulazizsaleh <32594301+abdulazizsaleh@users.noreply.github.com>
2018-03-06 03:07:10 +0300

    fix last update

--- imports/projectView.php
+++ imports/projectView.php
@@ -1,15 +1,15 @@
-<div class="container well">
-  <?php
-    $qry = "SELECT * FROM view";
-    $result = mysqli_query($db , $qry);
-    while($row = mysqli_fetch_array($result)){
-      echo '<img src="data:image;base64,'.base64_encode($row['pic']).'" class="img-responsive" style="width:100%"/>';
-      echo '<div class="">';
-      echo '<h3>  '.$row['title'].'</h3>';
-      echo '<h4>rate: '.$row['rate'].'</h4>';
-      echo '</div>';
-      echo '<p> '.$row['brief'].'</p>';
-      echo '<h4> views: '.$row['rate'].' </h4>';
-    }
-  ?>
-</div>
+<?php
+  $qry = "SELECT * FROM view";
+  $result = mysqli_query($db , $qry);
+  while($row = mysqli_fetch_array($result)){
+    echo '<div class="container well">';
+    echo '<img src="data:image;base64,'.base64_encode($row['pic']).'" class="img-responsive" style="width:100%"/>';
+    echo '<div class="">';
+    echo '<h3>  '.$row['title'].'</h3>';
+    echo '<h4>rate: '.$row['rate'].'</h4>';
+    echo '</div>';
+    echo '<p> '.$row['brief'].'</p>';
+    echo '<h4> views: '.$row['rate'].' </h4>';
+    echo '</div>';
+  }
+?>
