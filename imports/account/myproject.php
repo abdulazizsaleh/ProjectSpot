072a6bd45b0ac6622e1c2cfcfaf7ef1259442604
abdulazizsaleh <32594301+abdulazizsaleh@users.noreply.github.com>
2018-03-06 08:08:10 +0300

    connect user with project

--- imports/account/myproject.php
+++ imports/account/myproject.php
@@ -1,11 +1,27 @@
+
+<?php
+$result = mysqli_query($db,"select projectID from account where username = '".$username."'");
+$row = mysqli_fetch_array($result);
+$project = $row[0];
+?>
+
+
 <div class="col-md-9">
 <div class="row">
   <div class="col-md-12">
     <div class="well">
       <p><a href="./MyProject.php" class="btn btn-primary">My Project</a></p>
       <div class="list-group">
-        <a href="#" class="list-group-item list-group-item-action">project 1</a>
-        <a href="#" class="list-group-item list-group-item-action">project 2</a>
+        <?php
+        if ($project == 0){
+          echo '<h5 class="list-group-item list-group-item-action" >empty</h5>';
+        }else{
+          $result = mysqli_query($db,"select title from project where projectID = '".$project."'");
+          while($row = mysqli_fetch_array($result)){
+            echo '<a href="#" class="list-group-item list-group-item-action">'.$row[0].'</a>';
+          }
+        }
+        ?>
       </div>
     </div>
   </div>
