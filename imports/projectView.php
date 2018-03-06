4ecc43a6729602a86ffecec8b396ff1be5b2af2e
abdulazizsaleh <32594301+abdulazizsaleh@users.noreply.github.com>
2018-03-06 03:04:22 +0300

    update

--- imports/projectView.php
+++ imports/projectView.php
@@ -1,14 +1,15 @@
 <div class="container well">
   <?php
-  $qry = "SELECT * FROM view";
-  $result = mysqli_query($db , $qry);
-  $row = mysqli_fetch_array($result);
-  echo '<img src="data:image;base64,'.base64_encode($row['pic']).'" class="img-responsive" style="width:100%"/>';
+    $qry = "SELECT * FROM view";
+    $result = mysqli_query($db , $qry);
+    while($row = mysqli_fetch_array($result)){
+      echo '<img src="data:image;base64,'.base64_encode($row['pic']).'" class="img-responsive" style="width:100%"/>';
+      echo '<div class="">';
+      echo '<h3>  '.$row['title'].'</h3>';
+      echo '<h4>rate: '.$row['rate'].'</h4>';
+      echo '</div>';
+      echo '<p> '.$row['brief'].'</p>';
+      echo '<h4> views: '.$row['rate'].' </h4>';
+    }
   ?>
-  <div class="">
-    <h3><?php echo $row['title'] ?></h3>
-    <h4>rate: <?php echo $row['rate'] ?></h4>
-  </div>
-  <p><?php echo $row['brief'] ?></p>
-  <h4>views: <?php echo $row['rate'] ?></h4>
 </div>
