79a4f0c83aa6a45022b36dce9807d22c4a0d9e79
Bassam-Alhedaithi <36383683+Bassam-Alhedaithi@users.noreply.github.com>
2018-03-19 00:25:19 +0300

    edit account for logged in user

---
+++ editAcc.php
@@ -0,0 +1,154 @@
+<?php
+	
+	include './system/init.php';
+
+	session_start();
+	$user = $_SESSION['username'];
+
+	echo $user;
+
+$file = $_FILES['image']['tmp_name'];
+
+if(isset($file)){
+	
+	$image = addslashes(file_get_contents($_FILES['image']['tmp_name']));
+	$image_name = addslashes($_FILES['image']['name']);
+	$image_size = getimagesize($_FILES['image']['tmp_name']);
+
+	if($image_size==TRUE){
+			if($update_image = mysqli_query($db,"UPDATE account set image = '$image' WHERE username = '".$user."'"))
+			{
+
+			
+			$qryyyy = "SELECT image FROM account WHERE username = '".$user."'";
+			$result = mysqli_query($db , $qryyyy);
+			
+			$row = mysqli_fetch_array($result);
+		}
+		else
+		{
+			mysqli_error($insert);
+		}
+	}
+	else{
+		
+		echo "thats not image";
+	}
+}
+else{
+	echo "please select image";
+}
+
+	
+if($user){
+		if($_POST['submit'])
+		{
+			$oldpassword = $_POST['oldpassword'];
+			$newpassword = $_POST['newpassword'];
+			$repeatnewpassword = $_POST['repeatnewpassword'];
+			$qry_password = " SELECT * FROM account WHERE username = '".$user."'" ;
+
+			$qry_rs = mysqli_query($db , $qry_password);
+			
+			
+			$row = mysqli_fetch_array($qry_rs);
+			
+			$oldpassworddb =  $row['password'];
+			
+			
+			
+			if($oldpassword == $oldpassworddb)
+			{
+				if($newpassword == $repeatnewpassword)
+				{
+					$qry_update = mysqli_query($db , " UPDATE account SET password = $newpassword WHERE username = '".$user."'");
+					
+					session_destroy();
+					
+					echo " Your password have changed ";
+					
+					die("<a href='login.php'>return</a>");
+				}
+				else{
+					echo"new password not matchingssss ";
+				}
+					
+			}
+			else{
+				echo" password not matching ";
+			}
+			
+			$DOB = $_POST['dob'];
+			
+			$update_dob  = mysqli_query($db , " UPDATE account SET DOB = $DOB WHERE username = '".$user."'");
+			
+		}
+
+}
+else{
+	echo"error ";
+}
+
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<style>
+.edit {
+    padding: 50px;
+    font-size: 20px;
+    text-align: center;
+   
+}
+</style>
+</head>
+<body>
+
+<div class="edit">
+
+		<?echo '<h3>your image :</h3><img src="data:image;base64,'.base64_encode($row['image']).'"/>';?>
+
+<form action="editAcc.php" method="POST" enctype="multipart/form-data">
+
+Your Image : <input type = "file" name="image">
+<br>
+Your used Password : <input type="text" placeholder=" enter your used password "  name="oldpassword">
+<br>
+Your new Password : <input type="text" placeholder=" enter your new password "  name="newpassword">
+<br>
+Confirm new Password : <input type="text" placeholder=" enter your new password again "  name="repeatnewpassword">
+<br>
+
+
+Date of Birth : <input type="date" name="dob">
+<br>
+
+Your used Email : <input type="email" placeholder = "<?= $row['email'] ?>"  name="oldemail">
+
+<br>
+Your new Email : <input type="email" value="" name="newemail">
+
+<br>
+Confirm new Email : <input type="email" value="" name="repeatnewemail">
+
+<br>
+
+Phone Number : <input type="number" value="<?  $row['phone_num'] ?>"  name="num">
+
+<br>
+
+ <input type="radio" name="gender" value="Male">male
+
+<br>
+
+<input type="radio" name="gender" value="Female">Female
+
+<br>
+
+<input type="submit" name="submit" value="Save">
+
+</form>
+</div>
+</body>
+</html>
