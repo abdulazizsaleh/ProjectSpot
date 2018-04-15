c4393cc80999caef174547636aefa56e157d98f0
Bassam-Alhedaithi <36383683+Bassam-Alhedaithi@users.noreply.github.com>
2018-04-15 22:37:34 +0300

    added cookies as enhancment for views

--- projectViewMoreDetail.php
+++ projectViewMoreDetail.php
@@ -9,23 +9,85 @@ if(isset($_GET['vID'])){
   die();
 }
 
-$qry = "SELECT * FROM view where ID =".$vID;
-$result = mysqli_query($db , $qry);
-$row = mysqli_fetch_array($result);
 
-// below the count function
+// added cookies below to prevent duplicate views 
 
-$old_count = $row['veiws'];
-$new_count = $old_count + 1;
-$update_count = mysqli_query($db,"UPDATE view SET veiws = $new_count WHERE ID = ".$vID);
+if (!isset($_COOKIE['counter'])) {
+   setcookie('counter',0);
+}
+
+// when the counter value reaches 1 it will go to the else part and stop updating views
+if (isset($_COOKIE['counter']) && $_COOKIE['counter'] == 0) {
+   $current_val = $_COOKIE['counter'];
+   $current_val++;
+   setcookie('counter',$current_val);
+   $qry = "SELECT * FROM view where ID =".$vID;
+   $result = mysqli_query($db , $qry);
+   $row = mysqli_fetch_array($result);
+   $old_count = $row['veiws'];
+   $new_count = $old_count + 1;
+   $update_count = mysqli_query($db,"UPDATE view SET veiws = $new_count WHERE ID = ".$vID);
+
+ 
+  
+} else {
+   echo 'you have already visited this page';
+}
+
+
+$qry2 = "SELECT * FROM view where ID =".$vID;
+$rslt = mysqli_query($db , $qry2);
+$row = mysqli_fetch_array($rslt);
+
+$new_count = $row['veiws'];
 
 ///////////////////////////////////////////////////////////////////////////////////////////
+
+/*
+$post_rating = $_POST['rating'];
+
+$find_data = mysqli_query($db , "SELECT * FROM view WHERE ID = 3");
+
+while($row2 = mysqli_fetch_array($find_data)){
+	
+	
+	$current_rating = $row2['rate'];
+	$current_hits = $row2['hits'];
+
+}
+
+$new_hits = $current_hits + 1;
+$update_hits = mysqli_query($db , "UPDATE view SET hits = $new_hits WHERE ID= 3");
+
+$pre_rating = $current_rating + $post_rating;
+$new_rating = $pre_rating / $new_hits;
+
+$update_rating = mysqli_query($db , "UPDATE view SET rate = ".$new_rating." WHERE ID = 3");
+*/
+
+
+////////////////////////////////////////////////////////////////////////////////////////////////
 ?>
 <div class="container-fluid">
   <div class="well col-sm-9">
     <img src="data:image;base64,<?= base64_encode($row['pic'])?>" class="img-responsive" style="width:100%"/>
     <div class="">
       <h3><?= $row['title']?></h3>
+	  <form action="ratess.php" method="POST">
+						  <select name="rating">
+						  <option>1</option>
+						  <option>2</option>
+						  <option>3</option>
+						  <option>4</option>
+						  <option>5</option>
+						  <option>6</option>
+						  <option>7</option>
+						  <option>8</option>
+						  <option>9</option>
+						  <option>10</option>
+		  </select>
+		  <input type="submit" value="Rate it!">
+		</form>
       <h4>rate:<?= $row['rate']?></h4>
     </div>
     <p> <?= $row['description'] ?></p>
