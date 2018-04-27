377e728611e3424c4674771c52029592eb019b86
abdulazizsaleh <32594301+abdulazizsaleh@users.noreply.github.com>
2018-04-27 18:42:46 +0300

    fix

--- imports/head.php
+++ imports/head.php
@@ -8,6 +8,5 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <script src="js/jquery-3.3.1.min.js"></script>
     <script src="js/bootstrap.min.js"></script>
-    </script>
   </head>
   <body>
